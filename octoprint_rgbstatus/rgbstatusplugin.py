adf46d3f4754b8fccb044286f83923fcb3da72d1
suprs0nic <79784826+suprs0nic@users.noreply.github.com>
2024-07-21 13:37:35 +0200

    Update rgbstatusplugin.py

--- octoprint_rgbstatus/rgbstatusplugin.py
+++ octoprint_rgbstatus/rgbstatusplugin.py
@@ -8,19 +8,19 @@ import json
 import os.path
 
 from octoprint.events import Events
-# from octoprint_lui.constants import LuiEvents
+#from octoprint_lui.constants import LuiEvents
 
 from constants import *
 
 class RgbStatusPlugin(SettingsPlugin, BlueprintPlugin, ShutdownPlugin):
     def __init__(self):
         self._heating = False
-        self._default_color = (0.0, 0.0, 1.0, 0.0)  # Blue
+        self._default_color = (0., 0., 1., 0.) # Blue
         self._lights_enabled = False
 
     def initialize(self):
         # Initialize with default color "off", and enable transitions of 200ms with a refresh rate of 20ms
-        spirgbleds.initialize(color=(0.0, 0.0, 0.0, 0.0), transitionsEnabled=True, transitionRefreshInterval=20, transitionTime=200)
+        spirgbleds.initialize(color=(0.0,0.0,0.0,0.0), transitionsEnabled=True, transitionRefreshInterval=20, transitionTime=200)
         spirgbleds.start()
 
         self._lights_enabled = self._settings.get_boolean(["lights_enabled"])
@@ -81,7 +81,7 @@ class RgbStatusPlugin(SettingsPlugin, BlueprintPlugin, ShutdownPlugin):
         getpattern = name + "_pattern"
         color = self._settings.get([getcolor])
         pattern = self._settings.get([getpattern])
-        return make_response(jsonify({"color": color, "pattern": pattern}))
+        return make_response(jsonify({ "color": color, "pattern": pattern }))
 
     @BlueprintPlugin.route("/getdefault/<string:name>", methods=["GET"])
     def getdefault(self, name):
@@ -90,14 +90,14 @@ class RgbStatusPlugin(SettingsPlugin, BlueprintPlugin, ShutdownPlugin):
         defaultvars = self.get_settings_defaults()
         color = defaultvars[getcolor]
         pattern = defaultvars[getpattern]
-        return make_response(jsonify({"color": color, "pattern": pattern}))
+        return make_response(jsonify({ "color": color, "pattern": pattern }))
 
     @BlueprintPlugin.route("/setdefault/all", methods=["GET"])
     def setdefault_all(self):
         defaultvars = self.get_settings_defaults()
-        # self._settings.set(defaultvars)
+        #self._settings.set(defaultvars)
         for key in defaultvars.keys():
-            self._settings.set([key], defaultvars.get(key))
+            self._settings.set([key],defaultvars.get(key))
         self._settings.save()
         self.restore()
         return make_response(jsonify({"Settings": defaultvars.get(key)}))
@@ -128,7 +128,7 @@ class RgbStatusPlugin(SettingsPlugin, BlueprintPlugin, ShutdownPlugin):
     def off(self):
         self._lights_enabled = False
         self.on_lights_off()
-
+        
         self._settings.set(["lights_enabled"], False)
         self._settings.save()
 
@@ -141,7 +141,7 @@ class RgbStatusPlugin(SettingsPlugin, BlueprintPlugin, ShutdownPlugin):
     # dict and lookup the color/pattern/target
 
     def on_lights_off(self):
-        self._send_color(RgbTarget.BOTH, RgbPatterns.CONSTANT, (0.0, 0.0, 0.0, 0.0))
+        self._send_color(RgbTarget.BOTH, RgbPatterns.CONSTANT, (0.0,0.0,0.0,0.0))
         self._logger.debug("Switching the lights off")
 
     def on_startup(self):
@@ -188,7 +188,7 @@ class RgbStatusPlugin(SettingsPlugin, BlueprintPlugin, ShutdownPlugin):
 
     # End color mapping
 
-    def _find_current_state(self, event=None, payload=None):
+    def _find_current_state(self, event = None, payload = None):
         """
         Find the current state of the printer, and set the light color
         according to it. Switches lights off if they are not enabled
@@ -196,14 +196,14 @@ class RgbStatusPlugin(SettingsPlugin, BlueprintPlugin, ShutdownPlugin):
 
         # Switch lights off if the lights are not enabled
         if not self._lights_enabled:
-            self.on_lights_off()
+            self.on_lights_off()            
         # First try to act based on an event
         elif event == Events.PRINT_DONE:
             self.on_finished()
         elif event == Events.PRINT_FAILED:
             self.on_error()
         else:
-            # In all other situations, actually determine the state
+            # In all other sitations, actually determine the state
             if self._printer.is_error():
                 self.on_error()
             elif self._printer.is_printing():
@@ -215,11 +215,68 @@ class RgbStatusPlugin(SettingsPlugin, BlueprintPlugin, ShutdownPlugin):
                 self.on_paused()
             else:
                 data = self._printer.get_current_data()
-                if not event == Events.FILE_SELECTED and data.get("progress", {}).get("completion", 0) == 100:
+                if not event == Events.FILE_SELECTED and data.get("progress",{}).get("completion", 0) == 100:
                     self.on_finished()
                 else:
                     self.on_idle()
 
     def _set_events(self):
         """
-        Sets
+        Sets the OctoPrint events on which to re-determine the state
+        """
+        state_updates = [Events.CLIENT_OPENED, Events.PRINT_STARTED, Events.PRINT_PAUSED, Events.PRINT_RESUMED,
+                         Events.PRINT_DONE, Events.PRINT_FAILED, Events.FILE_SELECTED, Events.CONNECTED, Events.DISCONNECTED, Events.ERROR]
+
+        for event in state_updates:
+            self._event_bus.subscribe(event, self._find_current_state)
+
+    def _send_color(self, target, pattern, color):
+        """
+        Sends the color to the SPI driver, if enabled. Otherwise keeps the lights off.
+        Accepts both RGB(W) tuples and hex codes
+        """
+
+        if not self._lights_enabled:
+            spirgbleds.set_constant_color(RgbTarget.BOTH, (0.0,0.0,0.0,0.0))
+
+        # Ensure we work with a tuple of four floats
+        if isinstance(color, str):
+            color = self._parse_hex(color)
+        elif len(color) == 3:
+            color = color + (0.0,) # By default, keep the white channel low
+
+        if pattern == RgbPatterns.CONSTANT:
+            spirgbleds.set_constant_color(target, color)
+        elif pattern == RgbPatterns.FAST_PULSING:
+            spirgbleds.set_pulsing_color(target, color, 1000)
+        elif pattern == RgbPatterns.NORMAL_PULSING:
+            spirgbleds.set_pulsing_color(target, color, 2000)
+        elif pattern == RgbPatterns.SLOW_PULSING:
+            spirgbleds.set_pulsing_color(target, color, 4000)
+
+    def _parse_hex(self, hex_color):
+        """
+        Parse a hex color code and return a tuple with RGBW values (ranging 0.0 - 1.0).
+        Leading # are accepted. The fourth byte indicates white and may be omitted.
+        In this case, 0.0 is returned for the white channel.
+        """
+        # Remove any #
+        hex_color = hex_color.lstrip("#")
+        n = len(hex_color)
+
+        if n not in [6, 8]:
+            return self._default_color
+        
+        # Split bytestring into floats in 0.0 - 1.0 range
+        color = unhexlify(hex_color)
+
+        r  = color[0] / 255.
+        g  = color[1] / 255.
+        b  = color[2] / 255.
+
+        if n == 8:
+            w  = color[3] / 255.
+        else:
+            w = 0.
+
+        return (r, g, b, w)
